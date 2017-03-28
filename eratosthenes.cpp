683d2339bc3996c65b037f525e6f801edac5d469
doc-crowston <doc-crowston@users.noreply.github.com>
2017-03-28 16:38:52 +0100

    Update eratosthenes.cpp
    Store primes in a densely packed bit_array rather than one byte per true/false value.

--- eratosthenes.cpp
+++ eratosthenes.cpp
@@ -5,21 +5,86 @@
  * Copyright Dr Robert H Crowston, 2017, all rights reserved.
  * Use and redistribution is permitted under the BSD Licence available at https://opensource.org/licenses/bsd-license.php.
  *
- * Presently, this will only build with gcc-6.3.1 or later. To build, invoke g++ with the -std=c++17 flag.
+ * Presently, this will only build with gcc-7.0.0 (experimental version) or later. To build, invoke g++ with the -std=c++17 flag.
+ * Or, see it in action here: https://godbolt.org/g/eMocgO
  *
- * Bugs:
+ * Bugs and to do:
  *  o  This is not a true implementation of Eratosthenes’ method because every factor will be checked even if it was
  *     already encountered as a multiple of a lower factor.
  *  o  Array size is twice as big as necessary since evens are omitted.
- *  o  Won't build on clang.
  *
  */
 
-
-#include <array>
+#include <cassert>
+#include <climits>
+#include <cstddef>
 #include <cstdint>
+#include <initializer_list>
 #include <type_traits>
-#include <utility>    
+#include <utility> 
+
+namespace rhc
+{
+  namespace detail
+  {
+    template <typename T,
+    	typename = std::enable_if_t<std::is_integral<T>::value>
+    >
+    constexpr T ceil(float number)
+	{	// Note: std::ceil is not constexpr, so own implementation here.
+ 	   return (static_cast<float>(static_cast<T>(number)) == number) ?
+ 	       static_cast<T>(number) : static_cast<T>(number) + ((number > 0) ? 1 : 0);
+	}
+  }
+  
+  using std::size_t;
+  
+  template <size_t Size>
+  class bit_array
+  {
+    constexpr static size_t bits = Size;
+    constexpr static size_t bytes = detail::ceil<size_t>(static_cast<float>(bits) / CHAR_BIT);
+    std::byte storage[bytes];	// Raw backing storage.
+    
+    static constexpr size_t index_to_byte (const size_t index)
+    {	// Logical index to byte map.
+      return index/CHAR_BIT;
+    }
+    static constexpr size_t index_to_offset (const size_t index)
+    {	// Get the offset within one byte.
+    	return index - CHAR_BIT*(index/CHAR_BIT);
+    }
+        
+    public:
+    constexpr bit_array() : storage{} { ; }
+    
+    template <typename T,
+    	typename = std::enable_if_t<std::is_integral<T>::value>
+    >
+    constexpr bit_array (const T integer)
+    	: storage{*reinterpret_cast<const std::byte*>(&integer)}
+    { ; }
+    
+    constexpr bit_array (const std::initializer_list<bool>& list) : storage {}
+    {
+     	assert(list.size() <= bits);
+    	size_t index = 0;
+      	for (const auto& bit : list)
+        {
+        	storage[index_to_byte(index)] |= std::byte(bit << index_to_offset(index));
+          	++index;
+        } 
+    }
+    
+    constexpr bool operator[](const std::size_t index) const
+    {
+      return static_cast<bool>(
+       (storage[index_to_byte(index)] >> index_to_offset(index)) & std::byte(0x1)
+     );
+    }
+ 
+  };
+}
 
 namespace rhc::primes
 {
@@ -40,7 +105,7 @@ namespace rhc::primes
 	}	
 	
 	template <uint_t Size>
-	using table = std::array<bool, Size>;
+	using table = rhc::bit_array<Size>;
 
 	template <uint_t Size, uint_t Factor, index_t ... Is>
 	constexpr auto get_factor_table(std::index_sequence<Is ...> ) 
@@ -53,7 +118,7 @@ namespace rhc::primes
 	constexpr auto merge_factors(const table<Size> lhs, const table<Size> rhs, std::index_sequence<Is ...> )
 		-> table<Size>
 	{
-		return { lhs[Is] | rhs[Is] ... };
+		return { static_cast<bool>(lhs[Is] | rhs[Is]) ... };
 	}
 
 	template <uint_t Size, uint_t Factor
@@ -72,19 +137,6 @@ namespace rhc::primes
 			);
 		}
 	};
-
-	/*template <uint_t Size, uint_t Factor, 
-		typename = std::enable_if_t</* Factor is on composite list *//* >
-	>
-	struct merged_factor_table
-	{
-		constexpr static auto get()
-			-> table<Size>
-		{
-			// This factor already checked because it is a multiple of a previous factor; skip this factor and go to the next factor.
-			return merged_factor_table<Size, Factor+2>::get();
-		}
-	};*/
 
 	template <uint_t Size>
 	struct merged_factor_table<Size, Size>
@@ -108,7 +160,7 @@ namespace rhc::primes
 	}
 	
 	// Arbitrary check list.
-	static_assert(!check<7 >(0));
+	static_assert(!check<17>(0));
 	static_assert(!check<7 >(1));
 	static_assert( check<7 >(2));
 	static_assert( check<7 >(3));
@@ -123,6 +175,5 @@ namespace rhc::primes
 
 bool is_prime(const rhc::primes::uint_t num)
 {
-	return rhc::primes::check<101>(num);
+	return rhc::primes::check<257>(num);
 }
-
